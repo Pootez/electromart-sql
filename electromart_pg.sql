7a637593df62fb178114234b429eb322809aca59
Sondre Espeland <sondrenoe@gmail.com>
2025-05-21 13:16:46 +0200

    Update electromart_pg.sql

--- electromart_pg.sql
+++ electromart_pg.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 17.5
 -- Dumped by pg_dump version 17.5
 
--- Started on 2025-05-21 13:03:19
+-- Started on 2025-05-21 13:16:30
 
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
@@ -17,7 +17,7 @@ SET row_security = off;
 SET default_table_access_method = heap;
 
 --
--- TOC entry 217 (class 1259 OID 16521)
+-- TOC entry 217 (class 1259 OID 16618)
 -- Name: brand; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -29,7 +29,7 @@ CREATE TABLE public.brand (
 
 
 --
--- TOC entry 218 (class 1259 OID 16526)
+-- TOC entry 218 (class 1259 OID 16623)
 -- Name: brand_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -42,7 +42,7 @@ CREATE SEQUENCE public.brand_id_seq
 
 
 --
--- TOC entry 4974 (class 0 OID 0)
+-- TOC entry 4976 (class 0 OID 0)
 -- Dependencies: 218
 -- Name: brand_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
@@ -51,7 +51,7 @@ ALTER SEQUENCE public.brand_id_seq OWNED BY public.brand.id;
 
 
 --
--- TOC entry 219 (class 1259 OID 16527)
+-- TOC entry 219 (class 1259 OID 16624)
 -- Name: category; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -63,7 +63,7 @@ CREATE TABLE public.category (
 
 
 --
--- TOC entry 220 (class 1259 OID 16532)
+-- TOC entry 220 (class 1259 OID 16629)
 -- Name: category_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -76,7 +76,7 @@ CREATE SEQUENCE public.category_id_seq
 
 
 --
--- TOC entry 4975 (class 0 OID 0)
+-- TOC entry 4977 (class 0 OID 0)
 -- Dependencies: 220
 -- Name: category_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
@@ -85,7 +85,7 @@ ALTER SEQUENCE public.category_id_seq OWNED BY public.category.id;
 
 
 --
--- TOC entry 221 (class 1259 OID 16533)
+-- TOC entry 221 (class 1259 OID 16630)
 -- Name: orderitem; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -99,7 +99,7 @@ CREATE TABLE public.orderitem (
 
 
 --
--- TOC entry 222 (class 1259 OID 16536)
+-- TOC entry 222 (class 1259 OID 16633)
 -- Name: orderitem_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -112,7 +112,7 @@ CREATE SEQUENCE public.orderitem_id_seq
 
 
 --
--- TOC entry 4976 (class 0 OID 0)
+-- TOC entry 4978 (class 0 OID 0)
 -- Dependencies: 222
 -- Name: orderitem_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
@@ -121,7 +121,7 @@ ALTER SEQUENCE public.orderitem_id_seq OWNED BY public.orderitem.id;
 
 
 --
--- TOC entry 223 (class 1259 OID 16537)
+-- TOC entry 223 (class 1259 OID 16634)
 -- Name: orders; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -132,12 +132,12 @@ CREATE TABLE public.orders (
     orderdate timestamp with time zone NOT NULL,
     address character varying(255) NOT NULL,
     status character varying(30) DEFAULT 'pending'::character varying NOT NULL,
-    CONSTRAINT orders_status_check CHECK (((status)::text = ANY ((ARRAY['pending'::character varying, 'processing'::character varying, 'shipped'::character varying, 'delivered'::character varying, 'cancelled'::character varying, 'returned'::character varying])::text[])))
+    CONSTRAINT orders_status_check CHECK (((status)::text = ANY (ARRAY[('pending'::character varying)::text, ('processing'::character varying)::text, ('shipped'::character varying)::text, ('delivered'::character varying)::text, ('cancelled'::character varying)::text, ('returned'::character varying)::text])))
 );
 
 
 --
--- TOC entry 224 (class 1259 OID 16540)
+-- TOC entry 224 (class 1259 OID 16639)
 -- Name: orders_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -150,7 +150,7 @@ CREATE SEQUENCE public.orders_id_seq
 
 
 --
--- TOC entry 4977 (class 0 OID 0)
+-- TOC entry 4979 (class 0 OID 0)
 -- Dependencies: 224
 -- Name: orders_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
@@ -159,7 +159,7 @@ ALTER SEQUENCE public.orders_id_seq OWNED BY public.orders.id;
 
 
 --
--- TOC entry 225 (class 1259 OID 16541)
+-- TOC entry 225 (class 1259 OID 16640)
 -- Name: payment; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -169,12 +169,14 @@ CREATE TABLE public.payment (
     amount numeric(10,2) NOT NULL,
     paymentdate timestamp with time zone NOT NULL,
     status character varying(30) DEFAULT 'pending'::character varying NOT NULL,
-    CONSTRAINT payment_status_check CHECK (((status)::text = ANY ((ARRAY['pending'::character varying, 'paid'::character varying, 'failed'::character varying, 'refunded'::character varying])::text[])))
+    method character varying(30) DEFAULT 'Vipps'::character varying NOT NULL,
+    CONSTRAINT payment_method_check CHECK (((method)::text = ANY ((ARRAY['Credit Card'::character varying, 'Vipps'::character varying, 'Klarna'::character varying])::text[]))),
+    CONSTRAINT payment_status_check CHECK (((status)::text = ANY (ARRAY[('pending'::character varying)::text, ('paid'::character varying)::text, ('failed'::character varying)::text, ('refunded'::character varying)::text])))
 );
 
 
 --
--- TOC entry 226 (class 1259 OID 16544)
+-- TOC entry 226 (class 1259 OID 16645)
 -- Name: payment_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -187,7 +189,7 @@ CREATE SEQUENCE public.payment_id_seq
 
 
 --
--- TOC entry 4978 (class 0 OID 0)
+-- TOC entry 4980 (class 0 OID 0)
 -- Dependencies: 226
 -- Name: payment_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
@@ -196,7 +198,7 @@ ALTER SEQUENCE public.payment_id_seq OWNED BY public.payment.id;
 
 
 --
--- TOC entry 227 (class 1259 OID 16545)
+-- TOC entry 227 (class 1259 OID 16646)
 -- Name: product; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -212,7 +214,7 @@ CREATE TABLE public.product (
 
 
 --
--- TOC entry 228 (class 1259 OID 16550)
+-- TOC entry 228 (class 1259 OID 16651)
 -- Name: product_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -225,7 +227,7 @@ CREATE SEQUENCE public.product_id_seq
 
 
 --
--- TOC entry 4979 (class 0 OID 0)
+-- TOC entry 4981 (class 0 OID 0)
 -- Dependencies: 228
 -- Name: product_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
@@ -234,7 +236,7 @@ ALTER SEQUENCE public.product_id_seq OWNED BY public.product.id;
 
 
 --
--- TOC entry 229 (class 1259 OID 16551)
+-- TOC entry 229 (class 1259 OID 16652)
 -- Name: users; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -250,7 +252,7 @@ CREATE TABLE public.users (
 
 
 --
--- TOC entry 230 (class 1259 OID 16554)
+-- TOC entry 230 (class 1259 OID 16655)
 -- Name: users_userid_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -263,7 +265,7 @@ CREATE SEQUENCE public.users_userid_seq
 
 
 --
--- TOC entry 4980 (class 0 OID 0)
+-- TOC entry 4982 (class 0 OID 0)
 -- Dependencies: 230
 -- Name: users_userid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
@@ -272,7 +274,7 @@ ALTER SEQUENCE public.users_userid_seq OWNED BY public.users.userid;
 
 
 --
--- TOC entry 4772 (class 2604 OID 16555)
+-- TOC entry 4772 (class 2604 OID 16656)
 -- Name: brand id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -280,7 +282,7 @@ ALTER TABLE ONLY public.brand ALTER COLUMN id SET DEFAULT nextval('public.brand_
 
 
 --
--- TOC entry 4773 (class 2604 OID 16556)
+-- TOC entry 4773 (class 2604 OID 16657)
 -- Name: category id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -288,7 +290,7 @@ ALTER TABLE ONLY public.category ALTER COLUMN id SET DEFAULT nextval('public.cat
 
 
 --
--- TOC entry 4774 (class 2604 OID 16557)
+-- TOC entry 4774 (class 2604 OID 16658)
 -- Name: orderitem id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -296,7 +298,7 @@ ALTER TABLE ONLY public.orderitem ALTER COLUMN id SET DEFAULT nextval('public.or
 
 
 --
--- TOC entry 4775 (class 2604 OID 16558)
+-- TOC entry 4775 (class 2604 OID 16659)
 -- Name: orders id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -304,7 +306,7 @@ ALTER TABLE ONLY public.orders ALTER COLUMN id SET DEFAULT nextval('public.order
 
 
 --
--- TOC entry 4777 (class 2604 OID 16559)
+-- TOC entry 4777 (class 2604 OID 16660)
 -- Name: payment id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -312,7 +314,7 @@ ALTER TABLE ONLY public.payment ALTER COLUMN id SET DEFAULT nextval('public.paym
 
 
 --
--- TOC entry 4779 (class 2604 OID 16560)
+-- TOC entry 4780 (class 2604 OID 16661)
 -- Name: product id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -320,7 +322,7 @@ ALTER TABLE ONLY public.product ALTER COLUMN id SET DEFAULT nextval('public.prod
 
 
 --
--- TOC entry 4780 (class 2604 OID 16561)
+-- TOC entry 4781 (class 2604 OID 16662)
 -- Name: users userid; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -328,7 +330,7 @@ ALTER TABLE ONLY public.users ALTER COLUMN userid SET DEFAULT nextval('public.us
 
 
 --
--- TOC entry 4955 (class 0 OID 16521)
+-- TOC entry 4957 (class 0 OID 16618)
 -- Dependencies: 217
 -- Data for Name: brand; Type: TABLE DATA; Schema: public; Owner: -
 --
@@ -338,7 +340,7 @@ INSERT INTO public.brand VALUES (2, 'Apple', 'Mainly smartphones');
 
 
 --
--- TOC entry 4957 (class 0 OID 16527)
+-- TOC entry 4959 (class 0 OID 16624)
 -- Dependencies: 219
 -- Data for Name: category; Type: TABLE DATA; Schema: public; Owner: -
 --
@@ -348,7 +350,7 @@ INSERT INTO public.category VALUES (2, 'Tablets', 'All tablets');
 
 
 --
--- TOC entry 4959 (class 0 OID 16533)
+-- TOC entry 4961 (class 0 OID 16630)
 -- Dependencies: 221
 -- Data for Name: orderitem; Type: TABLE DATA; Schema: public; Owner: -
 --
@@ -359,7 +361,7 @@ INSERT INTO public.orderitem VALUES (3, 1, 2, 1, 12999.00);
 
 
 --
--- TOC entry 4961 (class 0 OID 16537)
+-- TOC entry 4963 (class 0 OID 16634)
 -- Dependencies: 223
 -- Data for Name: orders; Type: TABLE DATA; Schema: public; Owner: -
 --
@@ -370,18 +372,18 @@ INSERT INTO public.orders VALUES (3, 1, 9999.00, '2025-03-03 00:00:00+01', 'Kong
 
 
 --
--- TOC entry 4963 (class 0 OID 16541)
+-- TOC entry 4965 (class 0 OID 16640)
 -- Dependencies: 225
 -- Data for Name: payment; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.payment VALUES (1, 2, 12999.00, '2025-02-02 00:00:00+01', 'pending');
-INSERT INTO public.payment VALUES (2, 1, 8999.00, '2025-01-01 00:00:00+01', 'pending');
-INSERT INTO public.payment VALUES (3, 3, 9999.00, '2025-03-03 00:00:00+01', 'pending');
+INSERT INTO public.payment VALUES (1, 2, 12999.00, '2025-02-02 00:00:00+01', 'pending', 'Vipps');
+INSERT INTO public.payment VALUES (2, 1, 8999.00, '2025-01-01 00:00:00+01', 'pending', 'Vipps');
+INSERT INTO public.payment VALUES (3, 3, 9999.00, '2025-03-03 00:00:00+01', 'pending', 'Vipps');
 
 
 --
--- TOC entry 4965 (class 0 OID 16545)
+-- TOC entry 4967 (class 0 OID 16646)
 -- Dependencies: 227
 -- Data for Name: product; Type: TABLE DATA; Schema: public; Owner: -
 --
@@ -392,7 +394,7 @@ INSERT INTO public.product VALUES (3, 'iPad 16', 'Flagship-Apple', 9999.00, 48, 
 
 
 --
--- TOC entry 4967 (class 0 OID 16551)
+-- TOC entry 4969 (class 0 OID 16652)
 -- Dependencies: 229
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: -
 --
@@ -403,7 +405,7 @@ INSERT INTO public.users VALUES (3, 'lise@nord.no', 'effddd46', 'Lise', 'Nordman
 
 
 --
--- TOC entry 4981 (class 0 OID 0)
+-- TOC entry 4983 (class 0 OID 0)
 -- Dependencies: 218
 -- Name: brand_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
@@ -412,7 +414,7 @@ SELECT pg_catalog.setval('public.brand_id_seq', 2, true);
 
 
 --
--- TOC entry 4982 (class 0 OID 0)
+-- TOC entry 4984 (class 0 OID 0)
 -- Dependencies: 220
 -- Name: category_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
@@ -421,7 +423,7 @@ SELECT pg_catalog.setval('public.category_id_seq', 2, true);
 
 
 --
--- TOC entry 4983 (class 0 OID 0)
+-- TOC entry 4985 (class 0 OID 0)
 -- Dependencies: 222
 -- Name: orderitem_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
@@ -430,7 +432,7 @@ SELECT pg_catalog.setval('public.orderitem_id_seq', 3, true);
 
 
 --
--- TOC entry 4984 (class 0 OID 0)
+-- TOC entry 4986 (class 0 OID 0)
 -- Dependencies: 224
 -- Name: orders_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
@@ -439,7 +441,7 @@ SELECT pg_catalog.setval('public.orders_id_seq', 3, true);
 
 
 --
--- TOC entry 4985 (class 0 OID 0)
+-- TOC entry 4987 (class 0 OID 0)
 -- Dependencies: 226
 -- Name: payment_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
@@ -448,7 +450,7 @@ SELECT pg_catalog.setval('public.payment_id_seq', 3, true);
 
 
 --
--- TOC entry 4986 (class 0 OID 0)
+-- TOC entry 4988 (class 0 OID 0)
 -- Dependencies: 228
 -- Name: product_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
@@ -457,7 +459,7 @@ SELECT pg_catalog.setval('public.product_id_seq', 3, true);
 
 
 --
--- TOC entry 4987 (class 0 OID 0)
+-- TOC entry 4989 (class 0 OID 0)
 -- Dependencies: 230
 -- Name: users_userid_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
@@ -466,7 +468,7 @@ SELECT pg_catalog.setval('public.users_userid_seq', 3, true);
 
 
 --
--- TOC entry 4784 (class 2606 OID 16563)
+-- TOC entry 4786 (class 2606 OID 16664)
 -- Name: brand idx_16424_primary; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -475,7 +477,7 @@ ALTER TABLE ONLY public.brand
 
 
 --
--- TOC entry 4786 (class 2606 OID 16565)
+-- TOC entry 4788 (class 2606 OID 16666)
 -- Name: category idx_16431_primary; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -484,7 +486,7 @@ ALTER TABLE ONLY public.category
 
 
 --
--- TOC entry 4789 (class 2606 OID 16567)
+-- TOC entry 4791 (class 2606 OID 16668)
 -- Name: orderitem idx_16438_primary; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -493,7 +495,7 @@ ALTER TABLE ONLY public.orderitem
 
 
 --
--- TOC entry 4792 (class 2606 OID 16569)
+-- TOC entry 4794 (class 2606 OID 16670)
 -- Name: orders idx_16443_primary; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -502,7 +504,7 @@ ALTER TABLE ONLY public.orders
 
 
 --
--- TOC entry 4796 (class 2606 OID 16571)
+-- TOC entry 4798 (class 2606 OID 16672)
 -- Name: payment idx_16448_primary; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -511,7 +513,7 @@ ALTER TABLE ONLY public.payment
 
 
 --
--- TOC entry 4800 (class 2606 OID 16573)
+-- TOC entry 4802 (class 2606 OID 16674)
 -- Name: product idx_16453_primary; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -520,7 +522,7 @@ ALTER TABLE ONLY public.product
 
 
 --
--- TOC entry 4803 (class 2606 OID 16575)
+-- TOC entry 4805 (class 2606 OID 16676)
 -- Name: users idx_16460_primary; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -529,7 +531,7 @@ ALTER TABLE ONLY public.users
 
 
 --
--- TOC entry 4787 (class 1259 OID 16576)
+-- TOC entry 4789 (class 1259 OID 16677)
 -- Name: idx_16438_orderid; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -537,7 +539,7 @@ CREATE INDEX idx_16438_orderid ON public.orderitem USING btree (orderid);
 
 
 --
--- TOC entry 4790 (class 1259 OID 16577)
+-- TOC entry 4792 (class 1259 OID 16678)
 -- Name: idx_16438_productid; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -545,7 +547,7 @@ CREATE INDEX idx_16438_productid ON public.orderitem USING btree (productid);
 
 
 --
--- TOC entry 4793 (class 1259 OID 16578)
+-- TOC entry 4795 (class 1259 OID 16679)
 -- Name: idx_16443_userid; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -553,7 +555,7 @@ CREATE INDEX idx_16443_userid ON public.orders USING btree (userid);
 
 
 --
--- TOC entry 4794 (class 1259 OID 16579)
+-- TOC entry 4796 (class 1259 OID 16680)
 -- Name: idx_16448_orderid; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -561,7 +563,7 @@ CREATE INDEX idx_16448_orderid ON public.payment USING btree (orderid);
 
 
 --
--- TOC entry 4797 (class 1259 OID 16580)
+-- TOC entry 4799 (class 1259 OID 16681)
 -- Name: idx_16453_brandid; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -569,7 +571,7 @@ CREATE INDEX idx_16453_brandid ON public.product USING btree (brandid);
 
 
 --
--- TOC entry 4798 (class 1259 OID 16581)
+-- TOC entry 4800 (class 1259 OID 16682)
 -- Name: idx_16453_categoryid; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -577,7 +579,7 @@ CREATE INDEX idx_16453_categoryid ON public.product USING btree (categoryid);
 
 
 --
--- TOC entry 4801 (class 1259 OID 16582)
+-- TOC entry 4803 (class 1259 OID 16683)
 -- Name: idx_16460_email; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -585,7 +587,7 @@ CREATE UNIQUE INDEX idx_16460_email ON public.users USING btree (email);
 
 
 --
--- TOC entry 4804 (class 2606 OID 16583)
+-- TOC entry 4806 (class 2606 OID 16684)
 -- Name: orderitem orderitem_ibfk_1; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -594,7 +596,7 @@ ALTER TABLE ONLY public.orderitem
 
 
 --
--- TOC entry 4805 (class 2606 OID 16588)
+-- TOC entry 4807 (class 2606 OID 16689)
 -- Name: orderitem orderitem_ibfk_2; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -603,7 +605,7 @@ ALTER TABLE ONLY public.orderitem
 
 
 --
--- TOC entry 4806 (class 2606 OID 16593)
+-- TOC entry 4808 (class 2606 OID 16694)
 -- Name: orders orders_ibfk_1; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -612,7 +614,7 @@ ALTER TABLE ONLY public.orders
 
 
 --
--- TOC entry 4807 (class 2606 OID 16598)
+-- TOC entry 4809 (class 2606 OID 16699)
 -- Name: payment payment_ibfk_1; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -621,7 +623,7 @@ ALTER TABLE ONLY public.payment
 
 
 --
--- TOC entry 4808 (class 2606 OID 16603)
+-- TOC entry 4810 (class 2606 OID 16704)
 -- Name: product product_ibfk_1; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -630,7 +632,7 @@ ALTER TABLE ONLY public.product
 
 
 --
--- TOC entry 4809 (class 2606 OID 16608)
+-- TOC entry 4811 (class 2606 OID 16709)
 -- Name: product product_ibfk_2; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -638,7 +640,7 @@ ALTER TABLE ONLY public.product
     ADD CONSTRAINT product_ibfk_2 FOREIGN KEY (categoryid) REFERENCES public.category(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
 
 
--- Completed on 2025-05-21 13:03:19
+-- Completed on 2025-05-21 13:16:31
 
 --
 -- PostgreSQL database dump complete
